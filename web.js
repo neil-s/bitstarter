effd118102391a59ec5da09dab7115a6541162dc
Neil Satra <ns532@cam.ac.uk>
2013-09-11 08:32:53 +0000

    web.js reads content from index.html

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World! 2');
+  var buffer = new Buffer(fs.readFile('index.html'));
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
